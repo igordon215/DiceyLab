4294b6b734f057999715e87332841ad76e7cfaa6
igordon215 <igordon215@gmail.com>
2024-07-10 23:00:23 -0400

    working on sim

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,27 @@
 public class Simulation {
 
+    Bins bin = new Bins(2, 12);
+
+    public static void main(String[] args) {
+        Simulation sim = new Simulation();
+        sim.runSimulation(2, 100);
+        sim.printDisplay();
+    }
 
 
+    private void runSimulation(Integer numberOfDice, Integer numOfRolls) {
+        Dice simDice = new Dice(numberOfDice);
+        for (int i =0; i < numOfRolls; i++) {
+            Integer sum = simDice.tossAndSum();
+            bin.incrementBin(sum);
+        }
+    }
+
+    public void printDisplay() {
+        String starTable = "";
+        for (int i = 2; i < 13; i++) {
+            starTable += i + " : " + bin.getBin(i) + "  <---LooK" + "\n";
+        }
+        System.out.println(starTable);
+    }
 }
