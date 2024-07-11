96f2ce6ebb863228d1fed5d8df23621700d63582
igordon215 <igordon215@gmail.com>
2024-07-10 20:48:58 -0400

    dice test/class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,25 @@
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+    private Integer numDice;
+
+    Dice(Integer numDice){
+        this.numDice = numDice;
+    }
+
+    public Integer tossAndSum() {
+        Random random = new Random();
+        Integer sum = 0;
+
+        for(int i=0; i < numDice; i++){
+            sum += random.nextInt(6)+1;
+        }
+        return sum;
+    }
+
+    public Integer getNumDice(){
+        return numDice;
+    }
 
 }
+
--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,5 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
 import static org.junit.Assert.*;
 
 public class DiceTest {
+
+
+    @Test
+    public void tossAndSum1() {
+        Dice a = new Dice(50);
+        Integer min = 50;
+        Integer max = 50 * 6;
+
+        Integer sum = a.tossAndSum();
+        Assert.assertTrue(sum >= min && sum <= max);
+    }
+
+
+
+    @Test
+    public void tossAndSum2(){
+        Dice a = new Dice(2);
+        Integer min = 2;
+        Integer max = 2 * 6;
+
+        Integer sum = a.tossAndSum();
+        Assert.assertTrue(sum >= min && sum <= max);
+    }
 
 }
