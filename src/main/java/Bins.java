035d069867fe74c86cc4f1106ae993fef6a9bde6
igordon215 <igordon215@gmail.com>
2024-07-10 21:11:28 -0400

    taking break@ bins

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,25 @@
 
 public class Bins {
+    private Integer[] bins;
 
+    Bins(Integer min, Integer max){
+        bins = new Integer[max -min +1];
+
+        for(int i = 0; i < bins.length; i++){
+            bins[i] = 0;
+        }
+    }
+
+
+
+    public Integer getBin(int i) {
+        return null;
+    }
+
+    public void incrementBin(int i) {
+    }
+
+    public Integer[] getBins() {
+        return null;
+    }
 }
--- src/test/java/BinsTest.java
+++ src/test/java/BinsTest.java
@@ -1,5 +1,44 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BinsTest {
+    @Test
+    public void incrementBin() {
+        Bins myBin = new Bins(2, 12);
 
+
+        //given
+        Integer before = myBin.getBin(10) + 1;
+
+        //when
+        myBin.incrementBin(10);
+
+        //then
+        Integer after = myBin.getBin(10);
+
+        Integer[] bin = myBin.getBins();
+        for (int i = 0; i < bin.length; i++) {
+            System.out.println(i + " counter : data in bin " + bin[i]);
+        }
+
+        Assert.assertEquals(before, after);
+    }
+
+    @Test
+    public void getBin() {
+        Bins myBin = new Bins(2, 12);
+
+        //given
+        Integer before = myBin.getBin(10) + 1;
+
+        //when
+        myBin.incrementBin(10);
+
+        //then
+        Integer after = myBin.getBin(10);
+
+        Assert.assertEquals(before, after);
+    }
 }
